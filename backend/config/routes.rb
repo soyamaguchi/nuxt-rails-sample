07c398454941b18ae7decc1cb468a774b172ae7a
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-09-29 21:52:22 +0900

    Add backend

---
+++ backend/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :lists
+    end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
