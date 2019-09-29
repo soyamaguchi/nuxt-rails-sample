becf77354829293664b18e1da5d781edfd9851e7
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-09-29 22:19:47 +0900

    Change backend Get /lists時に昇順で返す、Post /lists時にRailsでlocationさせない

--- backend/app/controllers/api/v1/lists_controller.rb
+++ backend/app/controllers/api/v1/lists_controller.rb
@@ -4,7 +4,7 @@ module Api::V1
 
     # GET /lists
     def index
-      @lists = List.all
+      @lists = List.all.order(:id)
 
       render json: @lists
     end
@@ -19,7 +19,7 @@ module Api::V1
       @list = List.new(list_params)
 
       if @list.save
-        render json: @list, status: :created, location: @list
+        render json: @list, status: :created
       else
         render json: @list.errors, status: :unprocessable_entity
       end
